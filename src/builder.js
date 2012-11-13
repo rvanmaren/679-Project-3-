9f6acae94ab155b50e5bc396e3fbe8c3a088f13f
Masanek <michael.masanek@gmail.com>
2012-11-13 16:06:08 -0600

    add zoom to builder with incremental speed. Grid and click to build next

--- src/builder.js
+++ src/builder.js
@@ -2,17 +2,32 @@ var UP = 0;
 var DOWN = 1;
 var LEFT = 2;
 var RIGHT = 3;
-
+var builderSpeed = 2;
+var zoomSpeed = 10;
 function Builder(position, scene_handle, camera_handle)
 {
 	this.keys  = [false,false,false,false];
 	this.scene = scene_handle;
 	this.camera = camera_handle;
 	this.position = position;
-	
+	this.speed = builderSpeed;
+	this.height = position.y;
 	//always look directly down
 	this.direction = new THREE.Vector3(0,-1,0);
 	
+	this.zoom = function(zoomKey)
+	{
+		if(zoomKey == "in")
+		{
+			this.position.y-=zoomSpeed;
+			this.speed-=2;
+		}
+		else
+		{
+			this.position.y+=zoomSpeed;
+			this.speed+=2;
+		}
+	}
 	this.key_down = function(keyEvent)
 	{
 		switch (event.keyCode){	
@@ -25,9 +40,14 @@ function Builder(position, scene_handle, camera_handle)
 			case 68:
 				this.keys[RIGHT] = true;
 				break;
-				
 			case 83:
 				this.keys[DOWN] = true;
+				break;
+			case 81:
+				this.zoom("out");
+				break;
+			case 69:
+				this.zoom("in");
 				break;
 		}
 	};
@@ -50,15 +70,15 @@ function Builder(position, scene_handle, camera_handle)
 	};
 	this.mousePosition = function(mouseMoveX, mouseMoveY)
 	{
-
+		
 	}
 	this.update = function(time)
 	{
 		var forward = this.keys[UP] ? (this.keys[DOWN] ? 0 : 1) : (this.keys[DOWN] ? -1 : 0); //1,0,-1
 		var sideways = this.keys[RIGHT] ? (this.keys[LEFT] ? 0 : 1) : (this.keys[LEFT] ? -1 : 0);
 		
-		this.position.x -= forward;
-		this.position.z -= sideways;
+		this.position.x -= forward*this.speed;
+		this.position.z -= sideways*this.speed;
 		//this.position.z += this.direction.z*forward;
 		this.camera.position.set(this.position.x, this.position.y, this.position.z);
 		//this.camera.lookAt(this.position.x + dir.x, this.position.y + dir.y, this.position.z + dir.x);
